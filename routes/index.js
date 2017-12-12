048cd621d2f2d7420758441ce62fbc558a2337b5
Patrick Odum <felix.oje@gmail.com>
2017-12-12 00:24:19 -0800

    implementing authentication

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,9 @@ var router = express.Router();
 var MongoClient = require('mongodb').MongoClient;
 var objectId = require('mongodb').ObjectID;
 var assert = require('assert');
+var AuthenticationController = require('../app/controllers/authentication');
+var passportService = require('../config/passport');
+var passport = require('passport');
 
 var uriTestDb = "mongodb://Felix-O:bustmup@cluster0-shard-00-00-3xrpm.mongodb.net:27017,cluster0-shard-00-01-3xrpm.mongodb.net:27017,cluster0-shard-00-02-3xrpm.mongodb.net:27017/data_db?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin";
 
