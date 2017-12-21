2f4b7371a678ff3199a4f11b74211ee3907ffe0e
Patrick Odum <felix.oje@gmail.com>
2017-12-20 18:07:39 -0800

    auth profile update

--- app/controllers/authentication.js
+++ app/controllers/authentication.js
@@ -132,7 +132,7 @@ exports.update = function(req, res, next){
       res.json(req.body);
     });
     /**/
-    res.json(userUpdates.firstname);
+    res.json(user.firstname);
 }
 
 exports.delete = function(req, res, next){
