4b22a52d07c448882253227c72a68f1c65f1d570
Patrick Odum <felix.oje@gmail.com>
2018-01-07 17:43:42 -0800

    user index

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -4,11 +4,13 @@ exports.users = function(req, res, next){
 
   User.find({}, function(err, users) {
     var userMap = {};
+    var userData;
 
     users.forEach(function(user) {
       //userMap[user._id] = user._id;
+      userData = user._id;
     });
 
-    res.send(users[]);
+    res.send(usersData);
   });
 }
