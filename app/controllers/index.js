f2d284903258b4cd6edfa0fa9e607d89aa2558d4
Patrick Odum <felix.oje@gmail.com>
2018-01-11 02:28:42 -0800

    user index

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -15,8 +15,8 @@ exports.users = function(req, res, next){
 
 
 exports.user = function(req, res, next){
-  var username = req.body;
-  res.send("username");
+  //var username = req.body;
+  res.json("username");
   /*User.find( {username: username} , 'firstname lastname username email role', function(err, user) {
     res.json(user);
   });/**/
