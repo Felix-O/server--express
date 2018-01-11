10abe8c85c6622631b0da2e8007739159bcdf7c2
Patrick Odum <felix.oje@gmail.com>
2018-01-11 00:33:03 -0800

    user index

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -4,22 +4,20 @@ exports.users = function(req, res, next){
 
   User.find({}, 'firstname lastname username email role', function(err, users) {
     var userMap = {};
-    //var userData;
 
-    users.forEach(function(user) {
+    /*users.forEach(function(user) {
       userMap[user._id] = user._id;
       //userData = user._id;
-    });
+    });/**/
 
     res.json(users);
   });
 }
 
+/*
 exports.user = function(req, res, next){
-
   var id = req.body;
-
   User.findById( id , 'firstname lastname username', function(err, user) {
     res.send(user);
   });
-}
+}/**/
