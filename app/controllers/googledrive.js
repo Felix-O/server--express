bf5a04c32e124483f991ac7f3972834577000cbc
Patrick Odum <felix.oje@gmail.com>
2018-01-13 04:03:47 -0800

    google drive api

--- app/controllers/googledrive.js
+++ app/controllers/googledrive.js
@@ -2,6 +2,7 @@ var fs = require('fs');
 var readline = require('readline');
 var google = require('googleapis');
 var googleAuth = require('google-auth-library');
+var TOKEN_PATH = require('../../drive-nodejs-quickstart');
 
 //const keys = require('../../client_secret');
 
@@ -11,9 +12,9 @@ var googleAuth = require('google-auth-library');
 // If modifying these scopes, delete your previously saved credentials
 // at ~/.credentials/drive-nodejs-quickstart.json
 var SCOPES = ['https://www.googleapis.com/auth/drive'];
-var TOKEN_DIR = (process.env.HOME || process.env.HOMEPATH ||
+//var TOKEN_DIR = (process.env.HOME || process.env.HOMEPATH ||
     process.env.USERPROFILE) + '/.credentials/';
-var TOKEN_PATH = TOKEN_DIR + 'drive-nodejs-quickstart.json';
+//var TOKEN_PATH = TOKEN_DIR + 'drive-nodejs-quickstart.json';
 
 console.log(TOKEN_PATH);
 
