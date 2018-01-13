3de3a0cf503e040ee8ae939c8dddda561c755899
Patrick Odum <felix.oje@gmail.com>
2018-01-12 18:28:34 -0800

    google drive api

--- app/controllers/googledrive.js
+++ app/controllers/googledrive.js
@@ -5,10 +5,7 @@ var googleAuth = require('google-auth-library');
 
 exports.getContents = function(req, res, next){/**/
   var auth = google.auth.OAuth2;
-  var drive = google.drive({
-    version: 'v3',
-    auth: auth
-  });/*
+  var drive = google.drive('v3');/*
   drive.files.export({
     fileId: "1q2VD0k1xStuqEkTYSXwTDusn6mpsutWt8FpoI9h9VGs",
     mimeType: 'text/html'//,
