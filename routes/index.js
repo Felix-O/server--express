a206fb57b3518e8ac5489669f51f0c7280aab2d6
Patrick Odum <felix.oje@gmail.com>
2018-01-12 17:52:21 -0800

    google drive api

--- routes/index.js
+++ routes/index.js
@@ -37,7 +37,7 @@ router.get('/test2', passportService.test2);
   router.post('/api/auth/delete', AuthenticationController.delete);
   router.get('/api/index/users', IndexController.users);
   router.post('/api/index/user', IndexController.user);
-  router.get('/api/googledrive/filecontents', quickStart.getFileContents());
+  //router.get('/api/googledrive/filecontents', quickStart.getFileContents());
 /**/
 
 router.get('/getData', function(req, res, next) {
