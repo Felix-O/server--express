656ae0e7c716f67d550fc8f2af04c5d1b95cd31b
Patrick Odum <felix.oje@gmail.com>
2018-01-14 03:18:07 -0800

    google drive api

--- app/controllers/googledrive.js
+++ app/controllers/googledrive.js
@@ -17,7 +17,7 @@ exports.getContents = function(req, res, next){
   // Load client secrets from a local file.
   //res.json(TOKEN_PATH);
   // Load client secrets from a local file.
-  fs.readFile('drive-nodejs-quickstart.json', function processClientSecrets(err, content) {
+  fs.readFile(TOKEN_PATH, function processClientSecrets(err, content) {
     if (err) {
       res.json('Error loading client secret file: ' + err);
       return;
