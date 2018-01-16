4de801db5880cc36651a2e680871d42f670acf05
Patrick Odum <felix.oje@gmail.com>
2018-01-16 03:53:39 -0800

    google login implementation

--- routes/index.js
+++ routes/index.js
@@ -13,7 +13,7 @@ var quickStart = require('./quickstart');
 var requireAuth = passport.authenticate('jwt', {session: false}),
   requireLogin = passport.authenticate('local', {session: false});
 
-//var googleAuthLogin = passport.authenticate('google', {session: false});
+var googleAuthLogin = passport.authenticate('google', {session: false});
 
 var uriTestDb = "mongodb://Felix-O:bustmup@cluster0-shard-00-00-3xrpm.mongodb.net:27017,cluster0-shard-00-01-3xrpm.mongodb.net:27017,cluster0-shard-00-02-3xrpm.mongodb.net:27017/data_db?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin";
 
